cf60aa27b578eabc4ab2e28973904d44a12c62ac
mavrikkk <42514734+mavrikkk@users.noreply.github.com>
2021-01-27 14:09:23 +0300

    Update water_heater.py

--- custom_components/ready4sky/water_heater.py
+++ custom_components/ready4sky/water_heater.py
@@ -15,7 +15,8 @@ from homeassistant.components.water_heater import (
 from homeassistant.const import (
     STATE_UNKNOWN,
     STATE_OFF,
-    TEMP_CELSIUS
+    TEMP_CELSIUS,
+    CONF_MAC
 )
 from homeassistant.helpers import entity_platform
 
@@ -53,7 +54,8 @@ COOKER_OPERATION_LIST.append(STATE_OFF)
 
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
-    kettler = hass.data[DOMAIN]["kettler"]
+    mac = config_entry.get(CONF_MAC)
+    kettler = hass.data[DOMAIN][mac]
 
     if kettler._type == 0 or kettler._type == 1 or kettler._type == 2:
         async_add_entities([RedmondWaterHeater(kettler)], True)
