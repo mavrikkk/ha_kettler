8eb5af2326b17e4d503453de06a250e9c7d7e0fa
mavrikkk <42514734+mavrikkk@users.noreply.github.com>
2021-01-28 14:23:36 +0300

    Update switch.py

--- custom_components/ready4sky/switch.py
+++ custom_components/ready4sky/switch.py
@@ -12,6 +12,8 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     config = config_entry.data
     mac = config.get(CONF_MAC)
     kettler = hass.data[DOMAIN][mac]
+    if kettler._type == 3:
+        async_add_entities([RedmondSwitchIon(kettler)], True)
     if kettler._type == 4:
         async_add_entities([RedmondSwitch(kettler)], True)
 
@@ -72,3 +74,66 @@ class RedmondSwitch(SwitchEntity):
     @property
     def unique_id(self):
         return f'{DOMAIN}[{self._kettler._mac}][{self._name}]'
+    
+    
+    
+    
+    
+class RedmondSwitchIon(SwitchEntity):
+
+    def __init__(self, kettler):
+        self._name = 'Switch ' + kettler._name
+        self._icon = 'mdi:flash'
+        self._kettler = kettler
+        self._ison = False
+
+
+
+    async def async_added_to_hass(self):
+        self._handle_update()
+        self.async_on_remove(async_dispatcher_connect(self._kettler.hass, 'ready4skyupdate', self._handle_update))
+
+    def _handle_update(self):
+        self._ison = False
+        if self._kettler._ion == '01':
+            self._ison = True
+        self.schedule_update_ha_state()
+
+    @property
+    def device_info(self):
+        return {
+            "connections": {
+                ("mac", self._kettler._mac)
+            }
+        }
+
+    @property
+    def should_poll(self):
+        return False
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def icon(self):
+        return self._icon
+
+    @property
+    def is_on(self):
+        return self._ison
+
+    @property
+    def available(self):
+        return True
+
+    async def async_turn_on(self, **kwargs):
+        await self._kettler.async_modeIon('01')
+
+    async def async_turn_off(self, **kwargs):
+        await self._kettler.async_modeIon('00')
+
+    @property
+    def unique_id(self):
+        return f'{DOMAIN}[{self._kettler._mac}][{self._name}]'
+    
