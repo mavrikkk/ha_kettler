92046127a5150a765ea1502f4468d866d1cbb15d
mavrikkk <42514734+mavrikkk@users.noreply.github.com>
2021-02-01 08:35:52 +0300

    Update fan.py

--- custom_components/ready4sky/fan.py
+++ custom_components/ready4sky/fan.py
@@ -3,10 +3,10 @@
 
 from . import DOMAIN
 
-from homeassistant.util.percentage import (
-    ordered_list_item_to_percentage,
-    percentage_to_ordered_list_item,
-)
+#from homeassistant.util.percentage import (
+#    ordered_list_item_to_percentage,
+#    percentage_to_ordered_list_item,
+#)
 
 from homeassistant.const import CONF_MAC
 from homeassistant.components.fan import (
@@ -15,7 +15,7 @@ from homeassistant.components.fan import (
 )
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
 
-ORDERED_NAMED_FAN_SPEEDS = ['01', '02', '03', '04', '05', '06']  # off is not included
+#ORDERED_NAMED_FAN_SPEEDS = ['01', '02', '03', '04', '05', '06']  # off is not included
 
 
 
@@ -33,7 +33,8 @@ class RedmondFan(FanEntity):
         self._icon = 'mdi:fan'
         self._kettler = kettler
         self._ison = False
-        self._perc = 0
+        #self._perc = 0
+        self._speed = '01'
 
 
 
@@ -43,26 +44,40 @@ class RedmondFan(FanEntity):
 
     def _handle_update(self):
         self._ison = False
-        if self._kettler._mode == '00' or self._kettler._status == '00':
-            self._perc = 0
+        if self._kettler._mode == '00':
+            self._speed = '01'
         else:
-            self._perc = ordered_list_item_to_percentage(ORDERED_NAMED_FAN_SPEEDS, self._kettler._mode)
+            self._speed = self._kettler._mode
+#        if self._kettler._mode == '00' or self._kettler._status == '00':
+#            self._perc = 0
+#        else:
+#            self._perc = ordered_list_item_to_percentage(ORDERED_NAMED_FAN_SPEEDS, self._kettler._mode)
         if self._kettler._status == '02':
             self._ison = True
         self.schedule_update_ha_state()
 
-    async def async_set_percentage(self, percentage: int) -> None:
-        if percentage == 0:
-            await self.async_turn_off()
+#    async def async_set_percentage(self, percentage: int) -> None:
+#        if percentage == 0:
+#            await self.async_turn_off()
+#        else:
+#            speed = percentage_to_ordered_list_item(ORDERED_NAMED_FAN_SPEEDS, percentage)
+#            await self._kettler.async_modeFan(speed)
+       
+    async def async_set_speed(self, speed: str) -> None:
+        if speed == '00':
+            await self._kettler.async_modeOff()
         else:
-            speed = percentage_to_ordered_list_item(ORDERED_NAMED_FAN_SPEEDS, percentage)
             await self._kettler.async_modeFan(speed)
-                
+            
     async def async_turn_on(self, speed: str = None, percentage: int = None, preset_mode: str = None, **kwargs,) -> None:
-        if percentage is not None:
-            await self.async_set_percentage(percentage)
+        if speed is not None:
+            await self.async_set_speed(speed)
         else:
-            await self.async_set_percentage(0)
+            await self.async_set_speed('01')
+#        if percentage is not None:
+#            await self.async_set_percentage(percentage)
+#        else:
+#            await self.async_set_percentage(0)
 
     async def async_turn_off(self, **kwargs) -> None:
         await self._kettler.async_modeOff()
@@ -97,8 +112,16 @@ class RedmondFan(FanEntity):
         return True
 
     @property
-    def percentage(self) -> int:
-        return self._perc
+    def speed(self):
+        return self._speed
+    
+    @property
+    def speed_list(self):
+        return ['01', '02', '03', '04', '05', '06']
+    
+#    @property
+#    def percentage(self) -> int:
+#        return self._perc
 
     @property
     def supported_features(self) -> int:
