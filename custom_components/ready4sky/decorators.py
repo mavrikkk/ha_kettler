fa9c608db1f40e0a28475b45ad5bdbbe5a2f5e68
mavrikkk <42514734+mavrikkk@users.noreply.github.com>
2021-05-19 12:53:25 +0300

    Create decorators.py

---
+++ custom_components/ready4sky/decorators.py
@@ -0,0 +1,19 @@
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+
+def retry(howmany):
+    def tryIt(func):
+        def f(*args, **kwargs):
+            last_exception = None
+            attempts = 0
+            while attempts < howmany:
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    attempts += 1
+                    last_exception = e
+            else:
+                _LOGGER.warning(f'{howmany} attempts of {func.__name__} failed. Reason: {last_exception}')
+        return f
+    return tryIt
